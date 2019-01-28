026596769fa2b6f390a6c12cb920e9af15d031ea
iuvbio <lyndonbjohn@gmail.com>
2019-01-28 21:08:53 +0100

    bump version and add email

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 setup(name="pandas_ods_reader",
       version=VERSION,
       description="Read in an ODS file and return it as a pandas.DataFrame",
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Data Processing :: IO',
+      ],
+      keywords='data io pandas ods',
       url="http://github.com/iuvbio/pandas_ods_reader",
       author="iuvbio",
-      author_email="",
+      author_email="cryptodemigod@protonmail.com",
       license="MIT",
       packages=find_packages(),
       zip_safe=False,
-      install_requires=["ezodf", "pandas"])
+      install_requires=["ezodf", "pandas"]
+      )
