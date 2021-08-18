3ddf3a19cfa1c3123a32eba8c5cf714f43727e9f
iuvbio <lukas@goja.me>
2021-08-18 23:30:13 +0200

    reorganize structure

---
+++ pandas_ods_reader/parsers/ods.py
@@ -0,0 +1,53 @@
+from collections import OrderedDict
+
+import pandas as pd
+
+
+def load_ods(doc, sheet_id, headers=True, columns=None):
+    # convert the sheet to a pandas.DataFrame
+    if not isinstance(sheet_id, (int, str)):
+        raise ValueError("Sheet id has to be either `str` or `int`")
+    if isinstance(sheet_id, str):
+        sheets = [sheet.name for sheet in doc.sheets]
+        if sheet_id not in sheets:
+            raise KeyError("There is no sheet named {}".format(sheet_id))
+        sheet_id = sheets.index(sheet_id) + 1
+    sheet = doc.sheets[sheet_id - 1]
+    df_dict = OrderedDict()
+    col_index = {}
+    for i, row in enumerate(sheet.rows()):
+        # row is a list of cells
+        if headers and i == 0 and not columns:
+            # columns as lists in a dictionary
+            columns = []
+            for cell in row:
+                if cell.value and cell.value not in columns:
+                    columns.append(cell.value)
+                else:
+                    column_name = cell.value if cell.value else "unnamed"
+                    # add count to column name
+                    idx = 1
+                    while "{}.{}".format(column_name, idx) in columns:
+                        idx += 1
+                    columns.append("{}.{}".format(column_name, idx))
+
+            df_dict = OrderedDict((column, []) for column in columns)
+            # create index for the column headers
+            col_index = {j: column for j, column in enumerate(columns)}
+            continue
+        elif i == 0:
+            columns = columns if columns else ([f"column.{j}" for j in range(len(row))])
+            # columns as lists in a dictionary
+            df_dict = OrderedDict((column, []) for column in columns)
+            # create index for the column headers
+            col_index = {j: column for j, column in enumerate(columns)}
+            if headers:
+                continue
+        for j, cell in enumerate(row):
+            if j < len(col_index):
+                # use header instead of column index
+                df_dict[col_index[j]].append(cell.value)
+            else:
+                continue
+    df = pd.DataFrame(df_dict)
+    return df
--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-version = None
-with open('pandas_ods_reader/__init__.py') as f:
-    for line in f.readlines():
-        if not line.startswith('__version__'):
-            continue
-        version = line.split(' = ')[1].strip()[1:-1]
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-  name="pandas_ods_reader",
-  version=version,
-  description="Read in an ODS file and return it as a pandas.DataFrame",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  classifiers=[
-    "Development Status :: 2 - Beta",
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python :: 3",
-    "Topic :: Utilities",
-  ],
-  keywords="data io pandas ods",
-  url="http://github.com/iuvbio/pandas_ods_reader",
-  author="iuvbio",
-  author_email="cryptodemigod@protonmail.com",
-  license="MIT",
-  packages=find_packages(),
-  zip_safe=False,
-  install_requires=["ezodf", "pandas", "lxml"],
-  tests_require=["pytest"]
-)
+setup()
