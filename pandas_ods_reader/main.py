2b87c80b65247d02f641b72b0f51e963d3eb5b8d
ljnsn <82611987+ljnsn@users.noreply.github.com>
2022-11-16 00:17:30 +0100

    Add skiprows parameter to docstring.

--- pandas_ods_reader/main.py
+++ pandas_ods_reader/main.py
@@ -25,6 +25,8 @@ def read_ods(file_or_path, sheet=1, headers=True, columns=None, skiprows=0):
         If `True`, then the first row is treated as the list of column names.
     columns : list, default None, optional
         A list of column names to be used as headers.
+    skiprows : int, default 0
+        The number of rows to skip before starting to read data.
 
     Returns
     -------
