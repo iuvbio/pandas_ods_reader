e9e4b3b2728ceb179c04559f2fa0d58b872597f0
ljnsn <82611987+ljnsn@users.noreply.github.com>
2022-11-16 01:41:44 +0100

    Add more type hints.

--- pandas_ods_reader/parsers/ods.py
+++ pandas_ods_reader/parsers/ods.py
@@ -1,21 +1,28 @@
+from pathlib import Path
+from typing import Any, Iterator, Union
+
 import ezodf
+from ezodf.document import FlatXMLDocument, PackagedDocument
 
 
-def get_doc(file_or_path):
+def get_doc(file_or_path: Path) -> Union[FlatXMLDocument, PackagedDocument]:
     return ezodf.opendoc(file_or_path)
 
 
-def get_rows(doc, sheet_id):
+def get_rows(
+    doc: Union[FlatXMLDocument, PackagedDocument],
+    sheet_id: Union[str, int],
+) -> Iterator[list[ezodf.Cell]]:
     if not isinstance(sheet_id, (int, str)):
         raise ValueError("Sheet id has to be either `str` or `int`")
     if isinstance(sheet_id, str):
-        sheets = [sheet.name for sheet in doc.sheets]
+        sheets: list[str] = [sheet.name for sheet in doc.sheets]
         if sheet_id not in sheets:
             raise KeyError("There is no sheet named {}".format(sheet_id))
         sheet_id = sheets.index(sheet_id) + 1
-    sheet = doc.sheets[sheet_id - 1]
+    sheet: ezodf.Sheet = doc.sheets[sheet_id - 1]
     return sheet.rows()
 
 
-def get_value(cell, parsed=False):
+def get_value(cell: ezodf.Cell, parsed: bool = False) -> tuple[Any, int]:
     return cell.value, 0
